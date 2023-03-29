18b6660e3feb9c0e91fdb72c11be2ad0a74f735e
Mohammad <mohammadhprp@gmail.com>
2023-03-29 23:28:21 +0330

    feat: create OTP table

---
+++ app/Models/OTP.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class OTP extends Model
+{
+    use HasFactory;
+
+    public const SMS_CHANNEL = 1;
+    public const EMAIL_CHANNEL = 2;
+
+    protected $fillable = [
+        'request_id',
+        'receiver',
+        'receiver_channel',
+        'password',
+        'expired_at'
+    ];
+
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->request_id = Str::uuid();
+        });
+        self::updating(function ($model) {
+            $model->request_id = Str::uuid();
+        });
+    }
+}
---
+++ database/migrations/2023_03_29_194639_create_o_t_p_s_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('o_t_p_s', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('request_id')->unique();
+            $table->string('receiver');
+            $table->integer('receiver_channel');
+            $table->string('password');
+            $table->timestamp('expired_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('o_t_p_s');
+    }
+};
