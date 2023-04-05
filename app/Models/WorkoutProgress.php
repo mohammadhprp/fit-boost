81e6e85434f73c7a152cea9932b1880e1b54eba5
Mohammad <mohammadhprp@gmail.com>
2023-04-05 12:00:44 +0330

    feat: create user workout progress table

---
+++ app/Models/WorkoutProgress.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class WorkoutProgress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'user_workout_id',
+      'title',
+      'description',
+      'started_at',
+      'ended_at',
+    ];
+
+
+    public function user_workout(): BelongsTo
+    {
+        return $this->belongsTo(UserWorkout::class);
+    }
+}
