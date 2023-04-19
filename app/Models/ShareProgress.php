577e292d4c6dc9774dee542f268f54ea602c9ec8
Mohammad <mohammadhprp@gmail.com>
2023-04-19 13:55:50 +0330

    feat: generate url in boot:creating

--- app/Models/ShareProgress.php
+++ app/Models/ShareProgress.php
@@ -2,16 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class ShareProgress extends Model
 {
     use HasFactory, SoftDeletes;
-
 
     protected $fillable = [
         'user_id',
@@ -34,5 +35,20 @@ class ShareProgress extends Model
     public function visits(): HasMany
     {
         return $this->hasMany(ShareProgressVisit::class, 'share_progress_id');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->url = Str::random(6);
+        });
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope('by_user', function (Builder $builder) {
+            $builder->where('user_id', auth()->id());
+        });
     }
 }
