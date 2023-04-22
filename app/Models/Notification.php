8232c79067bc6a19d97e53bcff076e46b7b13f10
Mohammad <mohammadhprp@gmail.com>
2023-04-22 13:58:02 +0330

    feat: create notification model and table

---
+++ app/Models/Notification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'remind_at'
+    ];
+
+    protected $casts = [
+        'notify_at' => 'datetime',
+        'read_at' => 'datetime'
+    ];
+
+    public function notificationable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
